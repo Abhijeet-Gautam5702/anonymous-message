fefe06d3a06c0d35115defc8d31b8f3e8bceeee5
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-09-23 19:47:27 +0530

    Add comments

--- src/models/message.models.ts
+++ src/models/message.models.ts
@@ -15,6 +15,12 @@ export const MessageSchema: Schema<Message> = new Schema(
   { timestamps: true }
 );
 
+/*
+  NEXT-JS IS AN EDGE-TIME FRAMEWORK?
+  NextJS is an edge-time framework, meaning it executes its operations at the edge of the network (i.e., closer to the client) and not at the origin server. This helps in reducing latency and improved performance. 
+
+  Due to this edge-time nature of Next-JS, the backend server isn't running all the time. Instead, it is being run only when needed. Therefore, we need to check if the Message Model is already present in the database or not. If not, then we create a new model.
+*/
 const MessageModel = mongoose.model("Message", MessageSchema);
 
 export default MessageModel;
--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,6 +1,6 @@
-import mongoose, { Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { UserModelInterface as User } from "@/types/user.types";
-import { MessageSchema } from "./Message.models";
+import { MessageSchema } from "./message.models";
 
 const UserSchema: Schema<User> = new Schema(
   {
@@ -43,6 +43,14 @@ const UserSchema: Schema<User> = new Schema(
   { timestamps: true }
 );
 
-const UserModel = mongoose.model("User", UserSchema);
+/*
+  NEXT-JS IS AN EDGE-TIME FRAMEWORK?
+  NextJS is an edge-time framework, meaning it executes its operations at the edge of the network (i.e., closer to the client) and not at the origin server. This helps in reducing latency and improved performance. 
+
+  Due to this edge-time nature of Next-JS, the backend server isn't running all the time. Instead, it is being run only when needed. Therefore, we need to check if the User Model is already present in the database or not. If not, then we create a new model.
+*/
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
 
 export default UserModel;
